41a7201b9b46534c802a49541c98edfbf0909b69
PSDuckie <psduckie@yahoo.com>
2018-12-07 11:48:14 -0500

    Deleted the old version of the project, as I am doing a full rewrite.

--- CharGen/src/charGen/ESOChar.java
+++
@@ -1,7 +0,0 @@
-package charGen;
-
-public class ESOChar {
-	public String faction;
-	public String charClass;
-	public String race;
-}
--- CharGen/src/charGen/STOChar.java
+++
@@ -1,7 +0,0 @@
-package charGen;
-
-public class STOChar {
-	public String faction;
-	public String charClass;
-	public String shipClass;
-}
